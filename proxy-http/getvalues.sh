4d0cfa43be02141a22169aeceed1b0838f5141e3
exburied <r-hamel@outlook.fr>
2017-10-30 17:17:11 +0000

    Adding values at first line of file

--- proxy-http/getvalues.sh
+++ proxy-http/getvalues.sh
@@ -1,6 +1,10 @@
-./coap-client-rasp -v 5 -m get -N coap://192.168.1.50/light | echo $(tail -n 1)  "[$(date "+%Y-%m-%d %H:%M:%S")]" >> values/light.dat
-./coap-client-rasp -v 5 -m get -N coap://192.168.1.50/temperature | echo $(tail -n 1)  "[$(date "+%Y-%m-%d %H:%M:%S")]" >> values/temperature.dat
-./coap-client-rasp -v 5 -m get -N coap://192.168.1.50/humidite | echo $(tail -n 1)  "[$(date "+%Y-%m-%d %H:%M:%S")]" >> values/humidite.dat
 
+for req in light temperature humidite
+
+do
+	touch values/$req.dat
+	data=$(./coap-client-rasp -v 5 -m get -N coap://192.168.1.50/$req | echo $(tail -n 1)  "[$(date "+%Y-%m-%d %H:%M:%S")]")
+	echo -e "$(echo $data)\n$(cat values/$req.dat)" > values/$req.dat
+done
 
 
