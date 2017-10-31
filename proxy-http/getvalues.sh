6818e1c05b6afd66bc7f496216165fc784d9fbc9
exburied <r-hamel@outlook.fr>
2017-10-31 09:59:59 +0000

    ++

--- proxy-http/getvalues.sh
+++ proxy-http/getvalues.sh
@@ -3,7 +3,7 @@ for req in light temperature humidite
 
 do
 	touch values/$req.dat
-	data=$(./coap-client-rasp -v 5 -m get -N coap://192.168.1.50/$req | echo $(tail -n 1) "\t[$(date "+%Y-%m-%d %H:%M:%S")]")
+	data=$(./coap-client-rasp -v 5 -m get -N coap://192.168.1.50/$req | echo $(tail -n 1) "\t$(date "+%H:%M:%S %d-%m-%Y")")
 	echo -e "$(echo $data)\n$(cat values/$req.dat)" > values/$req.dat
 done
 
