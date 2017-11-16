7cdcac04d725821e8ee8dbb6baa74fbd0bf73566
exBuried <r-hamel@outlook.fr>
2017-11-16 12:20:02 +0100

    Invalid Server Response

--- proxy-http/good_server.py
+++ proxy-http/good_server.py
@@ -2,88 +2,80 @@
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
+import socket
+import sys
+import logging
+from coapthon.client.helperclient import HelperClient
 
+logging.basicConfig()
+
+coap_host="192.168.1.50"
+coap_port=5683
+client=HelperClient(server=(host, port))
 
 class S(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+	def _set_headers(self):
+		self.send_response(200)
+		self.send_header('Content-type', 'text/html')
+		self.end_headers()
 
-    def do_GET(self):
-        self._set_headers()
-        path=self.path.split(":")
-        if (path[0]=="/") :
-       		with open("values/temperature.dat", "r") as f :
-       			res="Temperature\t" + f.readline()
-        		self.wfile.write(res)
-                with open("values/humidite.dat", "r") as f :
-                	res="Humidite\t" + f.readline()
-                	self.wfile.write(res)
-                with open("values/light.dat", "r") as f :
-                	res="Luminosite\t" + f.readline()
-                	self.wfile.write(res)
+	def do_GET(self):
+		light_val=
+		temp_val=
+		hum_val=
+		self._set_headers()
 
-	elif (path[0]=="/temperature") :
-		if (len(path)==1):
-			with open("values/temperature.dat", "r") as f :
-				self.wfile.write(f.readline())
-		else :
-			request_range=int(path[1])
-			res=""
-			with open("values/temperature.dat", "r") as f :
-				for i in range(0, request_range) :
-					res+=f.readline() +"\n"
+		path=self.path.split("/")
+		if (path[0]=="/") :
+			res="Temperature\t" + temp_val
+			self.wfile.write(res)
+			res="Humidite\t" + hum_val
+			self.wfile.write(res)
+			res="Luminosite\t" + light_val
 			self.wfile.write(res)
 
+		elif (path[0]="/update")
 
-	elif (path[0]=="/humidite") :
-		if (len(path)==1):
-			with open("values/humidite.dat", "r") as f :
-				self.wfile.write(f.readline())
+		elif (path[0]=="/temperature") :
+			if (len(path)==1):
+				self.wfile.write(temp_val)
+
+			else if 
+
+
+		elif (path[0]=="/humidite") :
+			if (len(path)==1):
+				self.wfile.write(hum_val)
+			else :
+
+
+		elif (path[0]=="luminosite") :
+			if (len(path)==1):
+				self.wfile.write(light_val)
+			else :
+
 		else :
-			request_range=int(path[1])
-			res=""
-			with open("values/humidite.dat", "r") as f :
-				for i in range(0, request_range) :
-					res+=f.readline() +"\n"
-			self.wfile.write(res)
+			self.wfile.write("404 : Try [ /temperatue | /humidite | /luminosite ]")
 
+	def do_HEAD(self):
+		self._set_headers()
 
-	elif (path[0]=="luminosite") :
-		if (len(path)==1):
-			with open("values/light.dat", "r") as f :
-				self.wfile.write(f.readline())
-		else :
-			request_range=int(path[1])
-			res=""
-			with open("values/light.dat", "r") as f :
-				for i in range(0, request_range) :
-					res+=f.readline() +"\n"
-			self.wfile.write(res)
-
-	else :
-		self.wfile.write("404 : Try [ /temperatue | /humidite | /luminosite ]")
-
-    def do_HEAD(self):
-        	self._set_headers()
-
-    def do_POST(self):
-        	self._set_headers()
-        	self.wfile.write("POST!")
+	def do_POST(self):
+		self._set_headers()
+		self.wfile.write("POST!")
 
 
 def run(server_class=HTTPServer, handler_class=S, port=80):
 
 	server_address = ('', port)
 	httpd = server_class(server_address, handler_class)
-	print 'Starting httpd...'
+	print('Starting httpd...')
 	httpd.serve_forever()
 
 if __name__ == "__main__":
 	from sys import argv
 
 	if len(argv) == 2:
-        	run(port=int(argv[1]))
-    	else:
-        	run(port=8080)
+		run(port=int(argv[1]))
+	else:
+		run(port=8080)
--- proxy-http/test.py
+++ proxy-http/test.py
@@ -1,8 +1,9 @@
 import socket
 import sys
-
+import logging
 from coapthon.client.helperclient import HelperClient
 
+logging.basicConfig()
 host="192.168.1.50"
 port=5683
 client=HelperClient(server=(host, port))
@@ -10,5 +11,6 @@ client=HelperClient(server=(host, port))
 
 path="temperature"
 response=client.get(path)
+print("didiejd")
 print(response.pretty_print())
 client.stop()
